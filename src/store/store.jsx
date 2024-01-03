5dc7f2375f17ec6cd50dd0cd550495f612845a97
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-01-04 03:00:25 +0700

    feat: add include user while updating

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -23,7 +23,7 @@ const persistedReducer = persistReducer(persistConfig, userReducer);
 
 const store = configureStore({
   reducer: {
-    user: persistedReducer,
+    user: persistedReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
